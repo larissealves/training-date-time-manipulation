3797d55d5984d30e61aff27948720a036c522d2b
larisse.alves <alves.larisser@gmail.com>
2026-08-15 13:32:04 +0700

    :sparkles: feat(): - Adicionado função para formatar os valores em Date e no padrão BR; - Adicionado função para ordenar lista de eventos; ⚠ ️ Obs.: horário ainda não foi ajustado.

---
+++ OrganizeEvents.js
@@ -0,0 +1,71 @@
+import { eventos } from "./eventsData.js";
+
+function showEvents() {
+    eventos.map(item => {
+        console.log(item);
+    })
+}
+
+function adicionarCampoDataHora() {
+    eventos.forEach(item => {
+        item.dataHora = null;
+        item.dataHoraFormatoBr = null;
+    });
+
+    const json = JSON.stringify(eventos);
+    //console.log(json);
+    return json;
+}
+
+function formatarDataHora() {
+    eventos.forEach(item => {
+
+        // ========== Data timestamp - segundos e milissegundos ========== 
+        const timestamp = Number(item.data);
+        if (!Number.isNaN(timestamp)) {
+            const tamanhoSringTimestamp = String(Math.trunc(timestamp)).length;
+            if (tamanhoSringTimestamp === 13) {
+                item.dataHora = new Date(timestamp);
+                item.dataHoraFormatoBr = item.dataHora.toLocaleString("pt-BR");
+            } else if (tamanhoSringTimestamp === 10) {
+                item.dataHora = new Date(timestamp * 1000);
+                item.dataHoraFormatoBr = item.dataHora.toLocaleString("pt-BR");
+            } 
+            return;
+        }
+
+        // ========== Data short format (/ / /): 12/12/12 ========== 
+        else if (typeof item.data === "string" && item.data.includes("/")) {
+            const [dia, mes, ano] = item.data.split("/");
+            const [horas, minutos] = item.hora.split(':');
+
+            const newDate = `${ano}-${mes}-${dia}`;
+            item.dataHora = new Date(newDate);
+            item.dataHoraFormatoBr = item.dataHora.toLocaleString("pt-BR");
+
+            return;
+        }
+
+        else {
+            item.dataHora = new Date(item.data);
+            item.dataHoraFormatoBr = item.dataHora.toLocaleString("pt-BR");
+        }
+    });
+
+    //console.log(eventos);
+    return eventos;
+}
+
+function listarEventosOrganizados() {
+    const lista = formatarDataHora();
+    
+     const listaOrdenada = lista.sort((a, b) => {
+        return b.dataHora - a.dataHora;
+    });
+
+    console.log(listaOrdenada);
+    return listaOrdenada;
+}
+
+adicionarCampoDataHora();
+listarEventosOrganizados();
---
+++ eventsData.js
@@ -0,0 +1,56 @@
+export const eventos = [
+  {
+    id: 1,
+    nome: "Reunião",
+    data: "2026-08-15",
+    hora: "09:30"
+  },
+  {
+    id: 2,
+    nome: "Dentista",
+    data: "15/08/2026",
+    hora: "14:00"
+  },
+  {
+    id: 3,
+    nome: "Academia",
+    data: "2026-08-21",
+    hora: "07:15"
+  },
+  {
+    id: 4,
+    nome: "Cinema",
+    data: "20/08/2026",
+    hora: "20:45"
+  },
+  {
+    id: 5,
+    nome: "Black Friday",
+    data: "1786794600", // 01/01/1970
+    hora: "00:01"
+  },
+  {
+    id: 6,
+    nome: "Black Friday II",
+    data: "1786794600000", // 15/08/2026
+    hora: "09:30"
+  },
+  {
+    id: 7,
+    nome: "Academia",
+    data: "2026-01-01",
+    hora: "10:25"
+  },
+  {
+    id: 7,
+    nome: "Academia",
+    data: "2026-05-15",
+    hora: "07:13"
+  },
+   {
+    id: 8,
+    nome: "Academia",
+    data: "2026-08-16",
+    hora: "09:00"
+  },
+];
